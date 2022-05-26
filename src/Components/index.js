8eed2a8a228de898a2adc71b05285e3bbda0e2b1
Eden Sahilé <edensahile28@gmail.com>
2022-05-26 14:06:35 +0200

    first commit

---
+++ src/Components/index.js
@@ -0,0 +1,13 @@
+const Navbar=()=>{}
+
+const Banner=()=>{}
+
+const BooksList = () => {};
+
+const BestSeller = () => {};
+
+const NewsRelease = () => {};
+
+const CardBook = () => {};
+
+const Footer = () => {};
